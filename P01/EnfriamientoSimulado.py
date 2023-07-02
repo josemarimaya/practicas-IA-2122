71aa1e94625c6be2aa55b8fd576aee56e59c7eee
Jose Maria <josemarimaya@gmail.com>
2023-07-02 12:42:17 +0200

    Finish frozen simulate

--- P01/EnfriamientoSimulado.py
+++ P01/EnfriamientoSimulado.py
@@ -41,6 +41,7 @@ class Problema_Busqueda_Local(object):
         """Devuelve la valoración de un estado. Es el valor a optimizar."""
         abstract
 
+
 """ Implementar la clase Viajante_BL de problemas del viajante para ser resueltos mediante búsqueda local. 
     La clase debe ser subclase de Problema_Busqueda_Local y contener además dos atributos de datos adicionales: 
     uno con la lista de las ciudades y otro con una función distancia que se va a usar para calcular 
@@ -72,8 +73,8 @@ class Viajante_BL(Problema_Busqueda_Local):
         return ciudades_permutadas
 
     """ Trabajamos con el estado que es el que contiene las ciudades que hay actualmente en el problema"""
-    def genera_sucesor(self, estado):
 
+    def genera_sucesor(self, estado):
         cantidad_ciudades = len(estado)
 
         ciudad_1 = random.choice(range(0, cantidad_ciudades))
@@ -85,11 +86,10 @@ class Viajante_BL(Problema_Busqueda_Local):
         return estado_resultado
 
     def valoracion(self, estado):
-
         sum_circuito = 0
         # sumamos la distancia de las ciudades del estado
-        for i in range(len(estado)-1):
-            sum_circuito = sum_circuito + self.distancia(estado[i], estado[i+1])
+        for i in range(len(estado) - 1):
+            sum_circuito = sum_circuito + self.distancia(estado[i], estado[i + 1])
 
         # por ultimo sumamos las distancias de los extremos ya que es un circuito los extremos están conectados
         sum_circuito = sum_circuito + self.distancia(estado[0], estado[-1])
@@ -100,29 +100,132 @@ class Viajante_BL(Problema_Busqueda_Local):
 """ La función distancia_euc2D calcula la distancias entre dos ciudades a partir de sus coordenadas"""
 
 
-def distancia_euc2D(c1,c2,coords):
+def distancia_euc2D(c1, c2, coords):
     """ Función que recibe dos ciudades y devuelve la distancia entre ellas,
     calculada mediante distancia euclidea en el plano. El tercer argumento de
     la función contiene las coordenadas de todas las ciudades del problema (en
     foma de lista o de diccionario)"""
-    coord_c1= coords[c1]
-    coord_c2= coords[c2]
-    return math.hypot(coord_c1[0]-coord_c2[0],coord_c1[1]-coord_c2[1])
+    coord_c1 = coords[c1]
+    coord_c2 = coords[c2]
+    return math.hypot(coord_c1[0] - coord_c2[0], coord_c1[1] - coord_c2[1])
 
 
-andalucia={"almeria": (409.5, 93),
-           "cadiz":(63, 57),
-           "cordoba": (198, 207),
-           "granada": (309, 127.5),
-           "huelva":  (3, 139.5),
-           "jaen":    (295.5, 192),
-           "malaga":  (232.5,  75),
-           "sevilla": ( 90, 153)}
+andalucia = {"almeria": (409.5, 93),
+             "cadiz": (63, 57),
+             "cordoba": (198, 207),
+             "granada": (309, 127.5),
+             "huelva": (3, 139.5),
+             "jaen": (295.5, 192),
+             "malaga": (232.5, 75),
+             "sevilla": (90, 153)}
+
+# Para devolver las distancias tendremos en cuenta los values
+
+distancia = andalucia.values()
+
+print(distancia)
+
+# Y para devolver las ciudades usaremos las claves
+
+capitales = andalucia.keys()
+
+print(capitales)
+
+# Todo ellos para usar el problema del viajante con andalucía
+
+viajante_andalucia = Viajante_BL(capitales,
+                                 lambda x, y: distancia_euc2D(x, y, andalucia))
+
+# Ahora generamos el estado inicial
+
+circuito = viajante_andalucia.genera_estado_inicial()
+
+print(circuito)
+
+# Y un sucesor
+
+# sucesor = viajante_andalucia.genera_sucesor(circuito)
+
+# Implementación de enfriamiento simulado
+
+"""Definir una función sorteo(p), que recibe una probabilidad p y 
+devuelve aleatoriamente True ó False, de tal manera que la probabilidad de devolver True sea precisamente p."""
 
 
+def sorteo(p):
+    aleatorio = random.random()
+    res = False
+    if aleatorio < p:
+        res = True
+    return res
 
 
+"""Definir una función aceptar_e_s(valor_candidata, valor_actual, T, mejor)
+    que implementa el mecanismo de aceptación de nuevos estados dentro del algoritmo de enfriamiento simulado, donde:
+
+    valor_candidata es la valoración del estado nuevo
+    valor_actual es la valoración del estado actual
+    T es la temperatura
+    mejor es la función que se usa para comprobar si un valor es mejor qaue otro
+    
+    La función debe devolver True o False, dependiendo de si la candidata se acepta como nueva actual o no."""
 
 
+# Definimos la funcion auxiliar de mejor
+
+def mejor(v1, v2):
+    return (v1 < v2)
 
 
+def aceptar_e_s(valor_candidata, valor_actual, T, mejor):
+    condicion_1 = mejor(valor_actual, valor_candidata)
+    # La función de la temperatura en enfriamiento simulado = math.exp(-abs(valor_candidata-valor_actual)/T)
+    condicion_2 = sorteo(math.exp(-abs(valor_candidata - valor_actual) / T))
+
+    # Se tiene que cumplir alguna condicion para aceptar a la candidata
+    return condicion_1 or condicion_2
+
+
+## IMPLEMENTACIÓN DE ENFRIAMIENTO SIMULADO ##
+
+"""Función que implementa el algoritmo de optimización mediante
+        enfriamiento simulado. Recibe como entrada:
+        - Un problema de búsqueda local (clase Problema_Busqueda_Local)
+        - Una temperatura inicial
+        - Un factor de descenso (para el programa de enfriamiento)
+        - Un número total de enfriamientos 
+        - Un número de iteraciones para cada temperatura"""
+
+
+def enfriamiento_simulado(problema, t_inicial, factor_descenso, n_enfriamientos, n_iteraciones):
+    actual = problema.genera_estado_inicial()
+
+    valor_actual = problema.valoracion(actual)
+
+    mejor = actual
+
+    valor_mejor = valor_actual
+
+    T = t_inicial
+
+    for _ in range(n_enfriamientos):
+
+        for _ in range(n_iteraciones):
+
+            candidata = problema.genera_sucesor()
+            valor_candidata = candidata.valoracion(candidata)
+
+            if problema.mejor(valor_candidata, valor_actual):
+
+                actual = candidata
+
+                valor_actual = valor_candidata
+
+                if problema.mejor(valor_actual, valor_mejor):
+                    mejor = actual
+
+                    valor_mejor = valor_actual
+
+        T = T * factor_descenso
+
+    return mejor, valor_mejor
